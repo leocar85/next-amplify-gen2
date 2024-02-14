be56c550903fd49fab8c0a1eb63b0df48889dd65
Leo <teonardo_xavier@gmail.com>
2024-02-14 16:32:33 -0300

    amplify server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,46 @@
 // app/page.tsx
-"use client";
 
-import TodoList from "@/components/TodoList";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
+import { revalidatePath } from "next/cache";
 
-function App() {
+import { AuthGetCurrentUserServer, cookiesClient } from "@/utils/amplify-utils";
+
+import Logout from "@/components/Logout";
+
+async function App() {
+  const user = await AuthGetCurrentUserServer();
+  const { data: todos } = await cookiesClient.models.Todo.list();
+
+  async function addTodo(data: FormData) {
+    "use server";
+    const title = data.get("title") as string;
+    await cookiesClient.models.Todo.create({
+      content: title,
+      done: false,
+      priority: "medium",
+    });
+    revalidatePath("/");
+  }
+
   return (
     <>
       <h1>Hello, Amplify 👋</h1>
-      <TodoList />
+      {user && <Logout />}
+      <form action={addTodo}>
+        <input type="text" name="title" style={pageStyles.inputStyle} />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <ul>
+        {todos && todos.map((todo) => <li key={todo.id}>{todo.content}</li>)}
+      </ul>
     </>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
+
+const pageStyles = {
+  inputStyle: {
+    backgroundColor: "black"
+  }
+}
